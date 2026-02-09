86df7329910de9747f6b03b35bce169de32ef935
Kingsley Eze <firebox@ikingsley.com>
2026-02-09 23:30:17 +0100

    chore: bump version to 1.0.3

--- Sources/FlagKit/Core/EventQueue.swift
+++ Sources/FlagKit/Core/EventQueue.swift
@@ -91,7 +91,7 @@ public struct AnalyticsEvent: Sendable {
         timestamp: Date = Date(),
         userId: String? = nil,
         sessionId: String? = nil,
-        sdkVersion: String = "1.0.2"
+        sdkVersion: String = "1.0.3"
     ) {
         self.eventType = eventType
         self.eventData = eventData
